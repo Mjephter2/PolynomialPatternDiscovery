95d22529475a63fb9f32d170072b633c5056b7c8
Jephter Maurice <mjephter2@gmail.com>
2021-11-08 11:15:12 -0500

    Update Main.java to use input file provided by user

--- Main.java
+++ Main.java
@@ -4,19 +4,19 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args){
-        PolynomialPatternDiscovery discov = new PolynomialPatternDiscovery("inputFile.txt");
-        PolynomialFunction H1 = discov.calculateH1();
-        PolynomialFunction H2 = discov.calculateH2();
-        PolynomialFunction H3 = discov.calculateH3();
+        PolynomialPatternDiscovery patternDiscov = new PolynomialPatternDiscovery(args[0]);
+        PolynomialFunction h1 = patternDiscov.calculateH1();
+        PolynomialFunction h2 = patternDiscov.calculateH2();
+        PolynomialFunction h3 = patternDiscov.calculateH3();
         PolynomialFunction f = new PolynomialFunction(new LinkedList<>(), "f");
-        f.add(H1);
-        f.add(H2);
-        f.add(H3);
+        f.add(h1);
+        f.add(h2);
+        f.add(h3);
         System.out.println(f);
         DecimalFormat df = new DecimalFormat("#.####");
         df.setRoundingMode(RoundingMode.CEILING);
-        for(int i = 0; i < discov.xyPairs.length; i++){
-            System.out.println("f(" + discov.xyPairs[i][0] + ") = " + df.format(f.evaluate(discov.xyPairs[i][0])));
+        for(int i = 0; i < patternDiscov.xyPairs.length; i++){
+            System.out.println("f(" + patternDiscov.xyPairs[i][0] + ") = " + df.format(f.evaluate(patternDiscov.xyPairs[i][0])));
         }
     }
 }
